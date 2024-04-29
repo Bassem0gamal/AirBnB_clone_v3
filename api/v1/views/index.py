ba8a7e6021d0ef5c5bb177c0a2b3642ffbf81e51
Bassem Gamal <bassemgamalosman12@gmail.com>
2024-04-29 21:11:04 +0000

    Test 2

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-
 """ Index """
 from models import storage
 from flask import jsonify
 from api.v1.views import app_views
 
+
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """ Status of API """
     return jsonify({"status": "OK"})
+
 
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def number_objects():
