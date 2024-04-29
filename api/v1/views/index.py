fc946ffc895b97396e9bdfc7e5e88a36d0266bdf
Bassem Gamal <bassemgamalosman12@gmail.com>
2024-04-29 20:53:10 +0000

    Task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 """ Index """
+from models import storgae
+from flask import jsonify
 from api.v1.views import app_views
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
