32a9d33482cfc4cd9211fee3730abb09de92b45c
Bassem Gamal <bassemgamalosman12@gmail.com>
2024-04-29 22:44:52 +0000

    Task 7 City

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """ Blueprint for API """
 from flask import Blueprint
 
@@ -7,3 +6,4 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 from api.v1.views.index import *
 from api.v1.views.states import *
+from api.v1.views.cities import *
