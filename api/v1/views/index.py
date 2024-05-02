d8c979c3e9a873f6c19e5433f735fb141bfe188a
Bassem Gamal <bassemgamalosman12@gmail.com>
2024-05-02 19:38:37 +0000

    Test

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,7 +3,12 @@
 from models import storage
 from flask import jsonify
 from api.v1.views import app_views
-
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
